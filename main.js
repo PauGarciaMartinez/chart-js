d7eb56bf0c06ceb5de88dd08fa2569bbb57ad65a
Pau Garcia Martínez <etpau@hotmail.es>
2021-10-25 23:05:03 +0200

    Add some configuration and styles to the line chart

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const ctx = document.querySelector('#myChart').getContext('2d');
 
+let delayed;
+
+// Gradient Fill
+
+let gradient = ctx.createLinearGradient(0, 0, 0, 400);
+gradient.addColorStop(0, 'rgba(58, 123, 213, 1');
+gradient.addColorStop(1, 'rgba(0, 210, 255, 0.3');
+
 const labels = [
   '2012',
   '2013',
@@ -17,6 +25,10 @@ const data = {
   datasets: [{
     data: [211, 326, 165, 350, 420, 370, 500, 375, 415],
     label: 'Minecraft Sales',
+    fill: true,
+    backgroundColor: gradient,
+    borderColor: '#fff',
+    pointBackgroundColor: 'rgb(189, 195, 199)',
   }]
 };
 
@@ -24,7 +36,29 @@ const config = {
   type: 'line',
   data: data, 
   options: {
-    responsive: true
+    radius: 3,
+    hitRadius: 30,
+    hoverRadius: 5,
+    responsive: true,
+    animation: {
+      onComplete: () => {
+        delayed = true;
+      },
+      delay: (context) => {
+        let delay = 0;
+        if (context.type === 'data' && context.mode === 'default' && !delayed) {
+          delay = context.dataIndex * 300 + context.datasetIndex * 100;
+        }
+        return delay;
+      }
+    },
+    scales: {
+      y: {
+        ticks: {
+          callback: (value) => `$${value}m`
+        }
+      }
+    }
   }
 };
 
